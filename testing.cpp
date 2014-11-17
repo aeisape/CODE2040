8436d7c01fb8fb20b47cefd336ba43ba6d15612e
aeisape <aeisape1@jhu.edu>
2014-11-17 13:41:09 -0500

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <stdio.h>
 
 int main(){
 	std::cout<<"this is a test.. github is challenging.."<<std::endl;
+	std::cout<<"This was edited on github!"<<endl;
+	
+	printf(" _______    \n");
+        printf("|#Strut |   \n");
+        printf("| \\('-')|   \n");
+        printf("|  (  (>|   \n");
+        printf("|  /   \\|   \n");
+        printf("|_______|   \n\n");
+	
 }
